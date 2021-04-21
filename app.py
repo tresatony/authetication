b978a30517c9238b8e65828a6cc44e5e1d2e82fc
tresatony <75714123+tresatony@users.noreply.github.com>
2021-04-21 10:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+            error = 'Login failed due to incorrect username and password.Please try again.'
         else:
             return render_template('index.html')
 #return redirect(url_for('home'))
